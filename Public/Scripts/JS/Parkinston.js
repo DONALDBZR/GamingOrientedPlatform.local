180e0a6125df33dae57ae5902aacd3464f4b842e
DONALDBZR <andygaspard@hotmail.com>
2022-11-22 23:14:21 +0400

    UPDATE 0.4 Mounting all the modules

--- Public/Scripts/JS/Parkinston.js
+++ Public/Scripts/JS/Parkinston.js
@@ -13,6 +13,16 @@ class Parkinston {
          * The ID of the body
          */
         this.__bodyId;
+        /**
+         * List of modules needed
+         */
+        this._modulesPath = [
+            "/Modules/React/18.2.0/react.production.min.js",
+            "/Modules/React/18.2.0/react-dom.production.min.js",
+            "/Modules/Babel/7.20.4/babel.min.js",
+            "/Modules/Font-Awesome/6.2.1/fontawesome.min.js",
+        ];
+        this.init();
     }
     /**
      * Request URI accessor method
@@ -42,4 +52,28 @@ class Parkinston {
     setBodyId(body_id) {
         this.__bodyId = body_id;
     }
+    /**
+     * Initializing the application
+     */
+    init() {
+        this.setRequestURI(window.location.pathname);
+        if (this.getRequestURI() == "/") {
+            this.setBodyId("Homepage");
+        } else {
+            this.setBodyId(this.getRequestURI().replaceAll("/", ""));
+        }
+        document.body.id = this.getBodyId();
+        this.mount();
+    }
+    /**
+     * Mounting the modules
+     */
+    mount() {
+        for (let index = 0; index < this._modulesPath.length; index++) {
+            const script = document.createElement("script");
+            script.src = this._modulesPath[index];
+            document.head.appendChild(script);
+        }
+    }
 }
+const application = new Parkinston();
