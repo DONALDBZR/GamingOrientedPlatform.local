072ebc5421815fbeaddc42b6bb586b0605763161
DONALDBZR <andygaspard@hotmail.com>
2022-11-25 15:18:22 +0400

    UPDATE 0.19.6 Adding the sql script

---
+++ Public/Scripts/SQL/Parkinston.sql
@@ -0,0 +1,16 @@
+-- Creating the Database
+CREATE DATABASE Parkinston;
+-- Creating the Passwords table
+CREATE TABLE Parkinston.Passwords (
+    PasswordsId INT PRIMARY KEY AUTO_INCREMENT,
+    PasswordsSalt VARCHAR(8),
+    PasswordsHash VARCHAR(256)
+);
+-- Creating the Users table
+CREATE TABLE Parkinston.Users (
+    UsersUsername VARCHAR(32) PRIMARY KEY,
+    UsersMailAddress VARCHAR(64),
+    UsersPassword INT,
+    UsersProfilePicture VARCHAR(512),
+    CONSTRAINT fkPasswordsUsers FOREIGN KEY (UsersPassword) REFERENCES Parkinston.Passwords (PasswordsId)
+);
