d7d508a45684aa1080b7ecbe66d89c3ad454463f
DONALDBZR <andygaspard@hotmail.com>
2022-11-26 14:36:42 +0400

    UPDATE 0.20.14 Debugging

--- Models/User.php
+++ Models/User.php
@@ -28,11 +28,11 @@ class User extends Password
     }
     public function getPasswordId()
     {
-        $this->getId();
+        parent::getId();
     }
     public function setPasswordId(int $Password_id)
     {
-        $this->setId($Password_id);
+        parent::setId($Password_id);
     }
     public function register()
     {
