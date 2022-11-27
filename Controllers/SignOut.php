77744729b19798eeb1e2fe5377abb4ff70bb0f54
DONALDBZR <andygaspard@hotmail.com>
2022-11-27 22:13:48 +0400

    UPDATE 0.33.6 Debugging

--- Controllers/SignOut.php
+++ Controllers/SignOut.php
@@ -1,6 +1,6 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/Password.php";
-require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Scripts/PHP/User.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/User.php";
 $User = new User();
 $User->logOut();
