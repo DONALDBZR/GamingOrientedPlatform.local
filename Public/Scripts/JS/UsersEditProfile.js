5fb1ad68d5b43e5a736edb48b314e1d2304ba2e5
DONALDBZR <andygaspard@hotmail.com>
2022-12-01 18:47:10 +0400

    UPDATE 0.36.2 Adding the components

--- Public/Scripts/JS/UsersEditProfile.js
+++ Public/Scripts/JS/UsersEditProfile.js
@@ -10,17 +10,41 @@ class Application extends React.Component {
         this.state = {
             /**
              * Username of the user
+             * @type {string}
              */
             username: "",
             /**
              * Mail Address of the user
+             * @type {string}
              */
             mailAddress: "",
             /**
              * Domain of the application
+             * @type {string}
              */
             domain: "",
+            /**
+             * Response's status
+             * @type {int}
+             */
+            status: 0,
+            /**
+             * Response's body
+             * @type {string}
+             */
+            message: "",
+            /**
+             * Url to be redirected
+             * @type {string}
+             */
+            url: "",
+            /**
+             * User's profile picture
+             */
+            profilePicture: "",
+            array: [],
         };
+        this.handleFileChange = this.handleFileChange.bind(this);
     }
     /**
      * Renders the components that are being returned
@@ -29,7 +53,7 @@ class Application extends React.Component {
         return [<Header />, <Main />, <Footer />];
     }
     /**
-     * Retireving the session's data that is stored as a JSON to be used in the rendering
+     * Retrieving the session's data that is stored as a JSON to be used in the rendering
      */
     retrieveData() {
         fetch("/Users/CurrentUser",
@@ -42,6 +66,54 @@ class Application extends React.Component {
                 mailAddress: data.mailAddress,
                 domain: data.domain,
             }));
+    }
+    /**
+     * Handling any change that is made in the interface
+     * @param {Event} event
+     */
+    handleFileChange(event) {
+        this.setState({
+            profilePicture: event.target.files,
+            array: [],
+        });
+    }
+    /**
+     * Handling the form submission
+     * @param {Event} event
+     */
+    handleSubmit(event) {
+        /**
+        * The amount of milliseconds that the registration process takes
+        */
+        const delay = 3760;
+        /**
+         * Using Form-Data to upload the file
+         */
+        const formData = new FormData();
+        for (let index = 0; index < this.state.profilePicture.length; index++) {
+            formData.append("image", this.state.profilePicture[index]);
+        }
+        event.preventDefault();
+        fetch("/Controllers/UserEditProfile.php", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((data) => this.setState({
+                success: data.success,
+                message: data.message,
+                url: data.url,
+            }))
+            .then(() => this.redirector(delay));
+    }
+    /**
+     * Redirecting the user to an intended url
+     * @param {int} delay 
+     */
+    redirector(delay) {
+        setTimeout(() => {
+            window.location.href = this.state.url;
+        }, delay);
     }
 }
 /**
@@ -85,60 +157,41 @@ class Main extends Application {
     render() {
         return (
             <main>
-                <header>
-                    <div id="profilePicture">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <div id="username">{this.state.username}</div>
-                </header>
-                <description>
-                    <div id="mailAddress">
-                        <label>Mail Address:</label>
-                        <div>{this.state.mailAddress}</div>
-                    </div>
-                    <div id="lolUsername">
-                        <label>League of Legends's Username:</label>
-                        <div>Darkness4869</div>
-                    </div>
-                </description>
+                <Form />
             </main>
         );
-    }
-    /**
-     * Methods to be run as soon as the component is mounted
-     */
-    componentDidMount() {
-        this.retrieveData();
     }
 }
 /**
  * The component that is the footer
  */
 class Footer extends Application {
+    render() {
+        return (
+            <footer>
+                Parkinston
+            </footer>
+        );
+    }
+}
+/**
+ * Form component
+ */
+class Form extends Main {
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <footer>
+            <form method="POST" enctype="multipart/form-data" onSubmit={this.handleSubmit.bind(this)}>
+                <label>You can customize your profile picture</label>
+                <input type="file" name="image" accept="image/*" files={this.state.profilePicture} onChange={this.handleFileChange.bind(this)} required />
                 <div>
-                    <a href={`/Users/Edit/Profile/${this.state.username}`}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        Edit
-                    </a>
+                    <button>Change Profile Picture</button>
                 </div>
-                <div>Parkinston</div>
-            </footer>
+                <div>{this.state.message}</div>
+            </form>
         );
-    }
-    /**
-     * Methods to be run as soon as the component is mounted
-     */
-    componentDidMount() {
-        this.retrieveData();
     }
 }
 // Rendering the page
