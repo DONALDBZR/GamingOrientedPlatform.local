59bde54ee996a1259d392e6e29abe4ed6cf2b7b4
DONALDBZR <andygaspard@hotmail.com>
2022-12-04 12:48:10 +0400

    UPDATE 0.38.38 Fetching the data from Riot API

--- Models/LeagueOfLegends.php
+++ Models/LeagueOfLegends.php
@@ -2,7 +2,7 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Environment.php";
 class LeagueOfLegends
 {
-    private string $playerUniversallyUniqueIdentifier;
+    private string | null $playerUniversallyUniqueIdentifier;
     private string $gameName;
     private string $tagLine;
     public function __construct()
@@ -12,7 +12,7 @@ class LeagueOfLegends
     {
         return $this->playerUniversallyUniqueIdentifier;
     }
-    public function setPlayerUniversallytUniqueIdentifier(string $player_universally_unique_identifier)
+    public function setPlayerUniversallyUniqueIdentifier(string | null $player_universally_unique_identifier)
     {
         $this->playerUniversallyUniqueIdentifier = $player_universally_unique_identifier;
     }
@@ -32,11 +32,26 @@ class LeagueOfLegends
     {
         $this->tagLine = $tag_line;
     }
-    // public function retrieveData(string $game_name, string $tag_line)
-    // {
-    //     $this->setGameName($game_name);
-    //     $this->setTagLine($tag_line);
-    //     $riotAccountApiRequest = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" . $this->getGameName() . "/" . $this->getTagLine() . "?api_key=" . Environment::RiotAPIKey;
-    //     return $response;
-    // }
+    public function retrieveData(string $game_name, string $tag_line)
+    {
+        $this->setGameName($game_name);
+        $this->setTagLine($tag_line);
+        $riotAccountApiRequest = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" . $this->getGameName() . "/" . $this->getTagLine() . "?api_key=" . Environment::RiotAPIKey;
+        if ($this->getHttpResponseCode($riotAccountApiRequest) == 200) {
+            $riotAccountApiResponse = json_decode(file_get_contents($riotAccountApiRequest));
+            $this->setPlayerUniversallyUniqueIdentifier($riotAccountApiResponse->puuid);
+        }
+        $response = array(
+            "httpResponseCode" => $this->getHttpResponseCode($riotAccountApiRequest),
+            "playerUniversallyUniqueIdentifier" => $this->getPlayerUniversallyUniqueIdentifier(),
+            "gameName" => $this->getGameName(),
+            "tagLine" => $this->getTagLine()
+        );
+        return json_encode($response);
+    }
+    public function getHttpResponseCode(string $request_uniform_resource_locator)
+    {
+        $headers = get_headers($request_uniform_resource_locator);
+        return substr($headers[0], 9, 3);
+    }
 }
