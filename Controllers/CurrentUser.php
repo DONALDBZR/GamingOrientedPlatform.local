86db71bfbfeb7152587b26cd12a83191eecff3e5
DONALDBZR <andygaspard@hotmail.com>
2022-12-07 23:48:07 +0400

    UPDATE 0.39.72 Caching the data retrieved from the APIs

--- Controllers/CurrentUser.php
+++ Controllers/CurrentUser.php
@@ -1,8 +1,12 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
-$response = array(
-    "User" => $_SESSION['User'],
-    "Account" => $_SESSION['Account']
-);
+if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['User']['username']}.json")) {
+    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['User']['username']}.json"));
+} else {
+    $response = array(
+        "User" => $_SESSION['User'],
+        "Account" => $_SESSION['Account']
+    );
+}
 header('Content-Type: application/json', true, 200);
 echo json_encode($response);
