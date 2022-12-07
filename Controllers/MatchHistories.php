9d85478879551774e9d10fbae261f543f19679e4
DONALDBZR <andygaspard@hotmail.com>
2022-12-07 18:26:50 +0400

    UPDATE 0.39.54 Debuging

--- Controllers/MatchHistories.php
+++ Controllers/MatchHistories.php
@@ -2,4 +2,4 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/LeagueOfLegends.php";
 $LeagueOfLegends = new LeagueOfLegends();
-$LeagueOfLegends->getSummoner();
+$LeagueOfLegends->getMatchHistory();
