406d19db3099989faaa11f23585f7484ce4d9b33
DONALDBZR <andygaspard@hotmail.com>
2022-12-07 19:45:06 +0400

    UPDATE 0.39.58 Adding the champion icon

--- Models/LeagueOfLegends.php
+++ Models/LeagueOfLegends.php
@@ -147,7 +147,7 @@ class LeagueOfLegends
             $matchHistory = array();
             // $riotMatchApiRequest2 = "https://europe.api.riotgames.com/lol/match/v5/matches/" . $riotMatchApiResponse1[0] . "?api_key=" . Environment::RiotAPIKey;
             // $riotMatchApiResponse2 = json_decode(file_get_contents($riotMatchApiRequest2));
-            // $response = $riotMatchApiResponse1;
+            // $response = $riotMatchApiResponse2;
             for ($firstIndex = 0; $firstIndex < count($riotMatchApiResponse1); $firstIndex++) {
                 $riotMatchApiRequest2 = "https://europe.api.riotgames.com/lol/match/v5/matches/" . $riotMatchApiResponse1[$firstIndex] . "?api_key=" . Environment::RiotAPIKey;
                 $riotMatchApiResponse2 = json_decode(file_get_contents($riotMatchApiRequest2));
@@ -163,6 +163,7 @@ class LeagueOfLegends
                     $kdaRatio = ($riotMatchApiResponse2->info->participants[$puuidKey]->kills + $riotMatchApiResponse2->info->participants[$puuidKey]->assists) / 1;
                 }
                 $match = array(
+                    "champion" => $riotMatchApiResponse2->info->participants[$puuidKey]->championName,
                     "kda" => round($kdaRatio, 2)
                 );
                 array_push($matchHistory, $match);
