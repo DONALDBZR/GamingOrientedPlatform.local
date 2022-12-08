9ae04f27ae0ee221db8ec7a544b099cb90cca2fb
DONALDBZR <andygaspard@hotmail.com>
2022-12-09 00:17:00 +0400

    UPDATE 0.39.86 Debugging

---
+++ Controllers/LeagueOfLegendsHome.php
@@ -0,0 +1,26 @@
+<?php
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/LeagueOfLegends.php";
+$LeagueOfLegends = new LeagueOfLegends();
+if (json_decode(file_get_contents("php://input")) != null) {
+    if (!empty(json_decode(file_get_contents("php://input"))->lolSearch)) {
+        $LeagueOfLegends->getSummoner(json_decode(file_get_contents("php://input"))->lolSearch, $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+        $LeagueOfLegends->getMatchHistory(json_decode(file_get_contents("php://input"))->lolSearch, $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+    } else {
+        $response = array(
+            "status" => 1,
+            "url" => "/LeagueOfLegends/Home/{$_SESSION['Account']['LeagueOfLegends']['username']}",
+            "message" => "The form must be completely filled!"
+        );
+        header('Content-Type: application/json', true, 300);
+        echo json_encode($response);
+    }
+} else {
+    $response = array(
+        "status" => 1,
+        "url" => "/LeagueOfLegends/Home/{$_SESSION['Account']['LeagueOfLegends']['username']}",
+        "message" => "The form must be completely filled!"
+    );
+    header('Content-Type: application/json', true, 300);
+    echo json_encode($response);
+}
