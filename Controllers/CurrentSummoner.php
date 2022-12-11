e7f2e8a4d15e1d52d8a5e364a83b3b725b518266
DONALDBZR <andygaspard@hotmail.com>
2022-12-11 14:07:49 +0400

    UPDATE 0.40.24 Debugging

--- Controllers/CurrentSummoner.php
+++ Controllers/CurrentSummoner.php
@@ -2,10 +2,20 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/LeagueOfLegends.php";
 $LeagueOfLegends = new LeagueOfLegends();
-if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json")) {
-    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json"));
-    header('Content-Type: application/json', true, 200);
-    echo json_encode($response);
+if (str_contains($_SERVER['REQUEST_URI'], 'Home')) {
+    if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json")) {
+        $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json"));
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        $LeagueOfLegends->getSummoner($_SESSION['Account']['LeagueOfLegends']['gameName'], $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+    }
 } else {
-    $LeagueOfLegends->getSummoner($_SESSION['Account']['LeagueOfLegends']['gameName'], $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+    if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Search']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json")) {
+        $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Search']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.json"));
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        $LeagueOfLegends->getSummoner($_SESSION['Search']['LeagueOfLegends']['gameName'], $_SESSION['Search']['LeagueOfLegends']['tagLine']);
+    }
 }
