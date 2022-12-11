39a9ad7aa3de5b579412d93e21658efb19f3eb69
DONALDBZR <andygaspard@hotmail.com>
2022-12-11 15:10:47 +0400

    UPDATE 0.40.32 Debugging

--- Controllers/MatchHistories.php
+++ Controllers/MatchHistories.php
@@ -2,10 +2,21 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/LeagueOfLegends.php";
 $LeagueOfLegends = new LeagueOfLegends();
-if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json")) {
-    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json"));
-    header('Content-Type: application/json', true, 200);
-    echo json_encode($response);
+var_dump($_SERVER);
+if (str_contains($_SERVER['HTTP_REFERER'], "Home")) {
+    if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json")) {
+        $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json"));
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        $LeagueOfLegends->getMatchHistory($_SESSION['Account']['LeagueOfLegends']['gameName'], $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+    }
 } else {
-    $LeagueOfLegends->getMatchHistory($_SESSION['Account']['LeagueOfLegends']['gameName'], $_SESSION['Account']['LeagueOfLegends']['tagLine']);
+    if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Search']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json")) {
+        $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['Search']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']}.matchHistory.json"));
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        $LeagueOfLegends->getMatchHistory($_SESSION['Search']['LeagueOfLegends']['gameName'], $_SESSION['Search']['LeagueOfLegends']['tagLine']);
+    }
 }
