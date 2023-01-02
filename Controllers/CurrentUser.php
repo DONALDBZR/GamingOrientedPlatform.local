ce5be0f1b4820e60d149ddb252659518788c7e0e
DONALDBZR <andygaspard@hotmail.com>
2023-01-02 19:01:01 +0400

    UPDATE 1.9.4 Verifying that there is a session in the cache

--- Controllers/CurrentUser.php
+++ Controllers/CurrentUser.php
@@ -1,12 +1,9 @@
 <?php
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
-if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['User']['username']}.json")) {
-    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/{$_SESSION['User']['username']}.json"));
+if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$_SESSION['User']['username']}.json")) {
+    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$_SESSION['User']['username']}.json"));
 } else {
-    $response = array(
-        "User" => $_SESSION['User'],
-        "Account" => $_SESSION['Account']
-    );
+    header('Location: /Sign-Out');
 }
 header('Content-Type: application/json', true, 200);
 echo json_encode($response);
