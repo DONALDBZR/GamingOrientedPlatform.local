785d6aa62baa64e15ef47952b727bebb7f86f238
DONALDBZR <andygaspard@hotmail.com>
2023-01-20 11:33:44 +0400

    UPDATE 3.0.14 Debugging

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/User.php";
