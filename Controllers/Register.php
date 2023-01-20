77a2ee47e4ec8f251ac40599b2116ab4f15207cb
DONALDBZR <andygaspard@hotmail.com>
2023-01-20 12:07:56 +0400

    UPDATE 3.0.24 Debugging: Testing the controller

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -2,5 +2,8 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/User.php";
-$User = new User();
-$User->register();
+$request = json_decode(file_get_contents("php://input"));
+echo "TEST: <br />";
+var_dump($request);
+// $User = new User();
+// $User->register();
