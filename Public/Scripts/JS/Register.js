ea8b86bbacf6a60238b5719989a953180ef9b6ba
DONALDBZR <andygaspard@hotmail.com>
2023-01-20 12:00:40 +0400

    UPDATE 3.0.23 Debugging: Cancelling the redirector for locating the response needed

--- Public/Scripts/JS/Register.js
+++ Public/Scripts/JS/Register.js
@@ -73,7 +73,7 @@ class Application extends React.Component {
                 "Content-Type": "application/json",
             },
         })
-            .then((response) => response.body)
+            .then((response) => response.json())
             .then((data) => console.log(data));
         // .then(() => this.redirector(delay));
     }
