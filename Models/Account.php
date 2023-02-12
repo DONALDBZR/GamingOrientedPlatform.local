eeb5760ec9f2637583a89fb2be8708aa8e3a97e4
DONALDBZR <andygaspard@hotmail.com>
2023-02-12 16:20:38 +0400

    UPDATE 4.2.26 Removing unwanted data

--- Models/Account.php
+++ Models/Account.php
@@ -92,7 +92,7 @@ class Account extends User
                 $response = array(
                     "status" => $status,
                     "url" => "/Users/Accounts/{$_SESSION['User']['username']}",
-                    "message" => "This League of Legends account does not exist!"
+                    "message" => "This account does not exist!"
                 );
                 $headers = array(
                     "headers" => "Content-Type: application/json; X-XSS-Protection: 1; mode=block",
@@ -113,6 +113,54 @@ class Account extends User
      */
     public function managePlayerUnknownBattleGrounds(object $form)
     {
+        if (!isset($_SESSION['Account']['PlayerUnknownBattleGrounds'])) {
+            $status = $this->createPlayerUnknownBattleGroundsAccount($form->pubgUsername, $form->pubgPlatform);
+        } else {
+            $status = $this->updatePlayerUnknownBattleGroundsAccount($form->pubgUsername, $form->pubgPlatform);
+        }
+        switch ($status) {
+            case 0:
+                $response = array(
+                    "status" => $status,
+                    "url" => "/Users/Home/{$_SESSION['User']['username']}",
+                    "message" => "Your account has been added!"
+                );
+                $headers = array(
+                    "headers" => "Content-Type: application/json; X-XSS-Protection: 1; mode=block",
+                    "replace" => true,
+                    "responseCode" => 200
+                );
+                break;
+            case 1:
+                $response = array(
+                    "status" => $status,
+                    "url" => "/Users/Accounts/{$_SESSION['User']['username']}",
+                    "message" => "The form must be completely filled!"
+                );
+                $headers = array(
+                    "headers" => "Content-Type: application/json; X-XSS-Protection: 1; mode=block",
+                    "replace" => true,
+                    "responseCode" => 300
+                );
+                break;
+            case 11:
+                $response = array(
+                    "status" => $status,
+                    "url" => "/Users/Accounts/{$_SESSION['User']['username']}",
+                    "message" => "This account does not exist!"
+                );
+                $headers = array(
+                    "headers" => "Content-Type: application/json; X-XSS-Protection: 1; mode=block",
+                    "replace" => true,
+                    "responseCode" => 400
+                );
+                break;
+        }
+        $Response = (object) array(
+            "response" => $response,
+            "headers" => $headers
+        );
+        return $Response;
     }
     /**
      * Adding accounts
@@ -150,6 +198,32 @@ class Account extends User
      */
     public function edit(object $request)
     {
+        $this->setUsername($_SESSION['User']['username']);
+        $this->PDO->query("SELECT * FROM Accounts WHERE AccountsUser = :AccountsUser");
+        $this->PDO->bind(":AccountsUser", $this->getUsername());
+        $this->PDO->execute();
+        $this->setId($this->PDO->resultSet()[0]['AccountsId']);
+        if (!is_null($request->LeagueOfLegends->lolUsername) && !is_null($request->LeagueOfLegends->lolRegion)) {
+            $Response = $this->manageLeagueOfLegends($request->LeagueOfLegends);
+        } else if (!is_null($request->PlayerUnknownBattleGrounds->pubgUsername) && !is_null($request->PlayerUnknownBattleGrounds->pubgPlatform)) {
+            $Response = $this->managePlayerUnknownBattleGrounds($request->PlayerUnknownBattleGrounds);
+        } else {
+            $response = array(
+                "status" => 1,
+                "url" => "/Users/Accounts/{$_SESSION['User']['username']}",
+                "message" => "The form must be completely filled!"
+            );
+            $headers = array(
+                "headers" => "Content-Type: application/json; X-XSS-Protection: 1; mode=block",
+                "replace" => true,
+                "responseCode" => 300
+            );
+            $Response = (object) array(
+                "response" => $response,
+                "headers" => $headers
+            );
+        }
+        return $Response;
     }
     /**
      * Creating League Of Legends Accounts
@@ -199,6 +273,91 @@ class Account extends User
     public function updateLeagueOfLegendsAccount(?string $username, ?string $region)
     {
         if (!is_null($username) && !is_null($region)) {
+            $this->setUsername($_SESSION['User']['username']);
+            $this->LeagueOfLegends->setPlayerUniversallyUniqueIdentifier($_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']);
+            $this->PDO->query("SELECT * FROM Accounts WHERE AccountsLoL = :AccountsLoL AND AccountsUser = :AccountsUser");
+            $this->PDO->bind(":AccountsLoL", $this->LeagueOfLegends->getPlayerUniversallyUniqueIdentifier());
+            $this->PDO->bind(":AccountsUser", $this->getUsername());
+            $this->PDO->execute();
+            $this->setId($this->PDO->resultSet()[0]['AccountsId']);
+            if ($this->LeagueOfLegends->addAccount($username, $region) == 0) {
+                $this->PDO->query("INSERT INTO Accounts(AccountsLoL, AccountsUser) VALUES (:AccountsLoL, :AccountsUser)");
+                $this->PDO->bind(":AccountsLoL", $this->LeagueOfLegends->getPlayerUniversallyUniqueIdentifier());
+                $this->PDO->bind(":AccountsUser", $this->getUsername());
+                $this->PDO->execute();
+                $account = array(
+                    "LeagueOfLegends" => $_SESSION['LeagueOfLegends'],
+                    "PlayerUnknownBattleGrounds" => $_SESSION['PlayerUnknownBattleGrounds']
+                );
+                $_SESSION['Account'] = $account;
+                if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$this->getUsername()}.json")) {
+                    file_put_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$this->getUsername()}.json", "");
+                }
+                $data = array(
+                    "Client" => $_SESSION['Client'],
+                    "User" => $_SESSION['User'],
+                    "Account" => $_SESSION['Account']
+                );
+                $cacheData = json_encode($data);
+                $cache = fopen("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$_SESSION['User']['username']}.json", "w");
+                fwrite($cache, $cacheData);
+                fclose($cache);
+                return 0;
+            } else {
+                return 11;
+            }
+        } else {
+            return 1;
+        }
+    }
+    /**
+     * Creating Player Unknown Battle Grounds Accounts
+     * @param ?string $username
+     * @param ?string $region
+     * @return int
+     */
+    public function createPlayerUnknownBattleGroundsAccount(?string $username, ?string $platform)
+    {
+        if (!is_null($username) && !is_null($platform)) {
+            if ($this->PlayerUnknownBattleGrounds->addAccount($username, $platform) == 0) {
+                $this->PDO->query("INSERT INTO Accounts(AccountsLoL, AccountsUser) VALUES (:AccountsLoL, :AccountsUser)");
+                $this->PDO->bind(":AccountsLoL", $this->LeagueOfLegends->getPlayerUniversallyUniqueIdentifier());
+                $this->PDO->bind(":AccountsUser", $this->getUsername());
+                $this->PDO->execute();
+                $account = array(
+                    "LeagueOfLegends" => $_SESSION['LeagueOfLegends'],
+                    "PlayerUnknownBattleGrounds" => $_SESSION['PlayerUnknownBattleGrounds']
+                );
+                $_SESSION['Account'] = $account;
+                if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$this->getUsername()}.json")) {
+                    file_put_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$this->getUsername()}.json", "");
+                }
+                $data = array(
+                    "Client" => $_SESSION['Client'],
+                    "User" => $_SESSION['User'],
+                    "Account" => $_SESSION['Account']
+                );
+                $cacheData = json_encode($data);
+                $cache = fopen("{$_SERVER['DOCUMENT_ROOT']}/Cache/Session/Users/{$_SESSION['User']['username']}.json", "w");
+                fwrite($cache, $cacheData);
+                fclose($cache);
+                return 0;
+            } else {
+                return 11;
+            }
+        } else {
+            return 1;
+        }
+    }
+    /**
+     * Editing Player Unknown Battle Grounds Accounts
+     * @param ?string $username
+     * @param ?string $region
+     * @return int
+     */
+    public function updatePlayerUnknownBattleGroundsAccount(?string $username, ?string $platform)
+    {
+        if (!is_null($username) && !is_null($platform)) {
             $this->setUsername($_SESSION['User']['username']);
             $this->LeagueOfLegends->setPlayerUniversallyUniqueIdentifier($_SESSION['Account']['LeagueOfLegends']['playerUniversallyUniqueIdentifier']);
             $this->PDO->query("SELECT * FROM Accounts WHERE AccountsLoL = :AccountsLoL AND AccountsUser = :AccountsUser");
