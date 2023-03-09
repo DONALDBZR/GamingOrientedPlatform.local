5cd803845c09391d8e01dbf072525f6699d19784
DONALDBZR <andygaspard@hotmail.com>
2023-03-09 11:46:08 +0400

    UPDATE 4.4.7 Debugging the issue with the controller to return data from caching db

--- Controllers/CurrentPlayer.php
+++ Controllers/CurrentPlayer.php
@@ -4,7 +4,7 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/PlayerUnknownBattlegrounds.php"
 $PlayerUnknownBattleGrounds = new PlayerUnknownBattleGrounds();
 if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/PUBG/Users/Profiles/{$_SESSION['Account']['PlayerUnknownBattleGrounds']['identifier']}.json")) {
     $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/PUBG/Users/Profiles/{$_SESSION['Account']['PlayerUnknownBattleGrounds']['identifier']}.json"));
-    echo $response;
+    echo json_encode($response);
 } else {
     $PlayerUnknownBattleGrounds->getAccount($_SESSION['Account']['PlayerUnknownBattleGrounds']['playerName'], $_SESSION['Account']['PlayerUnknownBattleGrounds']['platform']);
 }
