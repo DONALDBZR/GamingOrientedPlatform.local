55b864f82ae7b9eb46368e35348b972ab57139c4
DONALDBZR <andygaspard@hotmail.com>
2023-03-19 14:41:02 +0400

    UPDATE 4.5.5 Adding the page for HTTP 503

---
+++ Public/Scripts/JS/HTTP503.js
@@ -0,0 +1,48 @@
+/**
+ * The Application that is going to be rendered in the DOM
+ */
+class Application extends React.Component {
+    /**
+     * Renders the components that are being returned
+     */
+    render() {
+        return [<Header />, <Main />, <Footer />];
+    }
+}
+/**
+ * The component that is the header
+ */
+class Header extends Application {
+    render() {
+        return <header>Parkinston</header>;
+    }
+}
+/**
+ * The component that is the main
+ */
+class Main extends Application {
+    /**
+     * Returns the user backward
+     */
+    return() {
+        history.back();
+    }
+    render() {
+        return (
+            <main>
+                <div class="Error">503</div>
+                <div id="message">Service Unavailable</div>
+            </main>
+        );
+    }
+}
+/**
+ * The component that is the footer
+ */
+class Footer extends Application {
+    render() {
+        return <footer>Parkinston</footer>;
+    }
+}
+// Rendering the page
+ReactDOM.render(<Application />, document.body);
