0510a25bf3e5ca31a4ba84e65613fb3934cc8914
DONALDBZR <andygaspard@hotmail.com>
2023-03-25 16:22:19 +0400

    UPDATE 4.5.88 Debugging

--- Public/Scripts/JS/UsersProfile.js
+++ Public/Scripts/JS/UsersProfile.js
@@ -4,103 +4,65 @@
 class Application extends React.Component {
     constructor(props) {
         super(props);
-        /**
-         * States of the properties of the component
-         */
         this.state = {
-            /**
-             * Username of the user
-             * @type {string}
-             */
-            username: "",
-            /**
-             * Mail Address of the user
-             * @type {string}
-             */
-            mailAddress: "",
-            /**
-             * Domain of the application
-             * @type {string}
-             */
-            domain: "",
-            /**
-             * User's profile picture
-             * @type {string}
-             */
-            profilePicture: "",
-            /**
-             * User's League of Legends username
-             * @type {string}
-             */
-            lolUsername: "",
-            /**
-             * User's League of Legends Region
-             * @type {string}
-             */
-            lolRegion: "",
-            /**
-             * User's Riot's ID
-             * @type {string}
-             */
-            riotId: "",
-            /**
-             * User's PUBG's account's ID
-             * @type {string}
-             */
-            pubgId: "",
-            /**
-             * User's PUBG's username
-             * @type {string}
-             */
-            pubgUsername: "",
-            /**
-             * User's PUBG's platform
-             * @type {string}
-             */
-            pubgPlatform: "",
+            User: {
+                username: "",
+                mailAddress: "",
+                profilePicture: "",
+            },
+            Accounts: {
+                LeagueOfLegends: {
+                    gameName: "",
+                },
+                PlayerUnknownBattleGrounds: {
+                    playerName: "",
+                },
+            },
         };
     }
     /**
      * Retrieving the session's data that is stored as a JSON to be used in the rendering
      */
-    retrieveData() {
+    getUser() {
         fetch("/Users/CurrentUser", {
             method: "GET",
         })
             .then((response) => response.json())
             .then((data) =>
                 this.setState({
-                    username: data.User.username,
-                    mailAddress: data.User.mailAddress,
-                    domain: data.User.domain,
-                    profilePicture: data.User.profilePicture,
-                    lolUsername: data.Account.LeagueOfLegends.gameName,
-                    lolRegion: data.Account.LeagueOfLegends.tagLine,
-                    riotId: data.Account.LeagueOfLegends
-                        .playerUniversallyUniqueIdentifier,
-                    pubgId: data.Account.PlayerUnknownBattleGrounds.identifier,
-                    pubgUsername:
-                        data.Account.PlayerUnknownBattleGrounds.playerName,
-                    pubgPlatform:
-                        data.Account.PlayerUnknownBattleGrounds.platform,
+                    User: {
+                        username: data.User.username,
+                        mailAddress: data.User.mailAddress,
+                        profilePicture: data.User.profilePicture,
+                    },
+                    Accounts: {
+                        LeagueOfLegends: {
+                            gameName: data.Account.LeagueOfLegends.gameName,
+                        },
+                        PlayerUnknownBattleGrounds: {
+                            playerName:
+                                data.Account.PlayerUnknownBattleGrounds
+                                    .playerName,
+                        },
+                    },
                 })
             );
     }
     /**
      * Verifying the state before rendering the link
-     * @returns {Application} Component
+     * @returns {object}
      */
     verifyUser_profilePicture() {
-        if (this.state.profilePicture != null) {
+        if (this.state.User.profilePicture != null) {
             return (
-                <a href={`/Users/Profile/${this.state.username}`}>
-                    <img src={this.state.profilePicture} />
+                <a href={`/Users/Profile/${this.state.User.username}`}>
+                    <img src={this.state.User.profilePicture} />
                 </a>
             );
         } else {
             return (
                 <a
-                    href={`/Users/Profile/${this.state.username}`}
+                    href={`/Users/Profile/${this.state.User.username}`}
                     class="fa fa-user"
                 ></a>
             );
@@ -108,20 +70,21 @@ class Application extends React.Component {
     }
     /**
      * Verifying whether there is a profile picture
-     * @returns {ProfilePicture}
+     * @returns {object}
      */
     verifyProfilePicture() {
-        if (this.state.profilePicture != null) {
-            return <img src={this.state.profilePicture} />;
+        if (this.state.User.profilePicture != null) {
+            return <img src={this.state.User.profilePicture} />;
         } else {
             return <i class="fa fa-user"></i>;
         }
     }
     /**
      * Verifying the state before applying style
+     * @returns {object|null}
      */
     verifyAccount_Riot_ID_styling() {
-        if (this.state.riotId == null) {
+        if (this.state.Accounts.LeagueOfLegends.gameName == null) {
             return {
                 display: "none",
             };
@@ -129,18 +92,15 @@ class Application extends React.Component {
     }
     /**
      * Verifying the state before applying style
+     * @returns {object|null}
      */
     verifyAccount_PUBG_ID_styling() {
-        if (this.state.pubgId == null) {
+        if (this.state.Accounts.PlayerUnknownBattleGrounds.playerName == null) {
             return {
                 display: "none",
             };
         }
     }
-    /**
-     * Renders the components that are being returned
-     * @returns {Application} Component
-     */
     render() {
         return [<Header />, <Main />, <Footer />];
     }
@@ -152,21 +112,15 @@ class Header extends Application {
     constructor(props) {
         super(props);
     }
-    /**
-     * Methods to be run as soon as the component is mounted
-     */
     componentDidMount() {
-        this.retrieveData();
+        this.getUser();
     }
-    /**
-     * @returns {Header} Component
-     */
     render() {
         return (
             <header>
                 <nav>
                     <div>
-                        <a href={`/Users/Home/${this.state.username}`}>
+                        <a href={`/Users/Home/${this.state.User.username}`}>
                             Parkinston
                         </a>
                     </div>
@@ -186,34 +140,28 @@ class Main extends Application {
     constructor(props) {
         super(props);
     }
-    /**
-     * Methods to be run as soon as the component is mounted
-     */
     componentDidMount() {
-        this.retrieveData();
+        this.getUser();
     }
-    /**
-     * @returns {Main} Component
-     */
     render() {
         return (
             <main>
                 <nav>
                     <div>
                         <a
-                            href={`/Users/Edit/Profile/${this.state.username}`}
+                            href={`/Users/Edit/Profile/${this.state.User.username}`}
                             class="fas fa-edit"
                         ></a>
                     </div>
                     <div>
                         <a
-                            href={`/Users/Accounts/${this.state.username}`}
+                            href={`/Users/Accounts/${this.state.User.username}`}
                             class="fa fa-user"
                         ></a>
                     </div>
                     <div>
                         <a
-                            href={`/Users/Security/${this.state.username}`}
+                            href={`/Users/Security/${this.state.User.username}`}
                             class="fa-solid fa-user-shield"
                         ></a>
                     </div>
@@ -223,19 +171,21 @@ class Main extends Application {
                         <div id="profilePicture">
                             {this.verifyProfilePicture()}
                         </div>
-                        <div id="username">{this.state.username}</div>
+                        <div id="username">{this.state.User.username}</div>
                     </header>
                     <description>
                         <div id="mailAddress">
                             <label>Mail Address:</label>
-                            <div>{this.state.mailAddress}</div>
+                            <div>{this.state.User.mailAddress}</div>
                         </div>
                         <div
                             id="lolUsername"
                             style={this.verifyAccount_Riot_ID_styling()}
                         >
                             <label>League of Legends's Username:</label>
-                            <div>{this.state.lolUsername}</div>
+                            <div>
+                                {this.state.Accounts.LeagueOfLegends.gameName}
+                            </div>
                         </div>
                         <div
                             id="pubgUsername"
@@ -244,7 +194,12 @@ class Main extends Application {
                             <label>
                                 Player Unknown Battle Grounds's Username:
                             </label>
-                            <div>{this.state.pubgUsername}</div>
+                            <div>
+                                {
+                                    this.state.Accounts
+                                        .PlayerUnknownBattleGrounds.playerName
+                                }
+                            </div>
                         </div>
                     </description>
                 </div>
@@ -259,15 +214,6 @@ class Footer extends Application {
     constructor(props) {
         super(props);
     }
-    /**
-     * Methods to be run as soon as the component is mounted
-     */
-    componentDidMount() {
-        this.retrieveData();
-    }
-    /**
-     * @returns {Footer} Component
-     */
     render() {
         return <footer>Parkinston</footer>;
     }
