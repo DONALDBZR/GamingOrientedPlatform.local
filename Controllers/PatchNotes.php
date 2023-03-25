25ce881bd2b51f415a76ee4c14fc60593260e257
DONALDBZR <andygaspard@hotmail.com>
2023-03-25 11:43:21 +0400

    UPDATE 4.5.78
    Allowing the controller to check for the date before processing for retrieving the version number of the game

--- Controllers/PatchNotes.php
+++ Controllers/PatchNotes.php
@@ -2,4 +2,15 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/LeagueOfLegends.php";
 $LeagueOfLegends = new LeagueOfLegends();
-$LeagueOfLegends->getPatchNotes();
+if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/Riot Games/Platform/Version.json")) {
+    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/Riot Games/Platform/Version.json"));
+    if (date("Y/m/d") < $response->renewOn) {
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        unlink("{$_SERVER['DOCUMENT_ROOT']}/Cache/Riot Games/Platform/Version.json");
+        $LeagueOfLegends->getPatchNotes();
+    }
+} else {
+    $LeagueOfLegends->getPatchNotes();
+}
