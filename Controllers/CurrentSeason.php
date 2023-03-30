040cafe5db65e19195deadd53bc3d2d344141d21
DONALDBZR <andygaspard@hotmail.com>
2023-03-30 07:11:55 +0400

    UPDATE 4.5.157
    The controller that will manage the data from the cache that is for the current user about his/her current season performance

---
+++ Controllers/CurrentSeason.php
@@ -0,0 +1,16 @@
+<?php
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/PlayerUnknownBattlegrounds.php";
+$PlayerUnknownBattleGrounds = new PlayerUnknownBattleGrounds();
+if (file_exists("{$_SERVER['DOCUMENT_ROOT']}/Cache/PUBG/Users/Seasons/{$_SESSION['Account']['PlayerUnknownBattleGrounds']['identifier']}.json")) {
+    $response = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/Cache/PUBG/Users/Seasons/{$_SESSION['Account']['PlayerUnknownBattleGrounds']['identifier']}.json"));
+    if (date("Y/m/d") < $response->renewOn) {
+        header('Content-Type: application/json', true, 200);
+        echo json_encode($response);
+    } else {
+        unlink("{$_SERVER['DOCUMENT_ROOT']}/Cache/PUBG/Users/Seasons/{$_SESSION['Account']['PlayerUnknownBattleGrounds']['identifier']}.json");
+        $PlayerUnknownBattleGrounds->getPlayer($_SESSION['Account']['PlayerUnknownBattleGrounds']['playerName'], $_SESSION['Account']['PlayerUnknownBattleGrounds']['platform']);
+    }
+} else {
+    $PlayerUnknownBattleGrounds->getPlayer($_SESSION['Account']['PlayerUnknownBattleGrounds']['playerName'], $_SESSION['Account']['PlayerUnknownBattleGrounds']['platform']);
+}
