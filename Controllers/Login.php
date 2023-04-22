e63a31062a24bc075fc9df771fb3311e4436e42e
DONALDBZR <andygaspard@hotmail.com>
2023-04-22 16:12:03 +0400

    UPDATE 4.5.191 Debugging

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -2,6 +2,5 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/User.php";
-$request = (object) json_decode(file_get_contents("php://input"));
 $User = new User();
-$User->login($request);
+$User->login($_POST[$_SERVER['REQUEST_URI']]);
