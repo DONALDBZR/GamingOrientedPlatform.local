51cd8d4e9bf4e6291a8c2348513ece8405ac568e
DONALDBZR <andygaspard@hotmail.com>
2023-04-23 12:48:30 +0400

    UPDATE 4.7.28 Documenting

--- Public/Scripts/JS/SignOut.js
+++ Public/Scripts/JS/SignOut.js
@@ -27,27 +27,30 @@ class Application extends React.Component {
     }
     /**
      * Retrieving the session's data that is stored as a JSON to be used in the rendering
+     * @returns {void}
      */
     retrieveData() {
         /**
          * The amount of milliseconds that the registration process takes
          */
         const delay = 800;
-        fetch("/Controllers/SignOut.php",
-            {
-                method: "GET"
-            })
+        fetch("/Controllers/SignOut.php", {
+            method: "GET",
+        })
             .then((response) => response.json())
-            .then((data) => this.setState({
-                status: data.status,
-                message: data.message,
-                url: data.url,
-            }))
+            .then((data) =>
+                this.setState({
+                    status: data.status,
+                    message: data.message,
+                    url: data.url,
+                })
+            )
             .then(() => this.redirector(delay));
     }
     /**
      * Redirecting the user to an intended url
-     * @param {int} delay 
+     * @param {int} delay
+     * @returns {void}
      */
     redirector(delay) {
         setTimeout(() => {
@@ -81,11 +84,7 @@ class Header extends Application {
         super(props);
     }
     render() {
-        return (
-            <header>
-                Parkinston
-            </header>
-        );
+        return <header>Parkinston</header>;
     }
 }
 /**
