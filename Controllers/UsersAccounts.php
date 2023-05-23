e2ee370975b216f3fa85aa82bf20192d2ec0405b
DONALDBZR <andygaspard@hotmail.com>
2023-05-23 18:29:44 +0400

    UPDATE 4.7.58 Correcting the routing

--- Controllers/UsersAccounts.php
+++ Controllers/UsersAccounts.php
@@ -4,4 +4,5 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/User.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Account.php";
 $Account = new Account();
+$_POST[$_SERVER['REQUEST_URI']] = (object) json_decode(file_get_contents("php://input"));
 $Account->manage($_POST[$_SERVER['REQUEST_URI']]);
