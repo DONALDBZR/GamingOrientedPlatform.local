21f16a66e98ac60ee4f92b01a7964cd7a3014165
DONALDBZR <andygaspard@hotmail.com>
2023-05-23 18:21:03 +0400

    UPDATE 4.7.55 Correcting the routing

--- Controllers/LoginVerification.php
+++ Controllers/LoginVerification.php
@@ -2,4 +2,5 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Password.php";
 $Password = new Password();
+$_POST[$_SERVER['REQUEST_URI']] = (object) json_decode(file_get_contents("php://input"));
 $Password->otpVerify($_POST[$_SERVER['REQUEST_URI']]);
